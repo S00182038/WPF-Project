cfe06f2077ea242c52646278c75705742d896f98
Arkadiusz Lesica <S00182038@mail.itsligo.ie>
2019-02-05 22:07:16 +0000

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,15 +11,19 @@ namespace Currency_Converter_App
         {
             InitializeComponent();
         }
-
+        //Open the registration window
         private void BtnSignUp_Click(object sender, RoutedEventArgs e)
         {
-
+            var reg = new Registration(); //create  new registration form.
+            reg.Show(); //show the new form.
+            this.Close(); // close the current form.
         }
 
         private void BtnSignIn_Click(object sender, RoutedEventArgs e)
         {
-
+            var signIn = new SignIn(); //create  new sign in form.
+            signIn.Show(); //show the new form.
+            this.Close(); // close the current form.
         }
     }
 }
